182fdca224cc6534a8637a8a31bed7727a147438
Felipe <macbookair@MacBook-Air-de-MacBook.local>
2020-07-21 10:48:42 -0300

    Push Notification implementado no android

--- SuperLogin.Android/MainActivity.cs
+++ SuperLogin.Android/MainActivity.cs
@@ -16,6 +16,11 @@ namespace SuperLogin.Droid
     [Activity(Label = "SuperLogin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        static readonly string TAG = "MainActivity";
+
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -25,6 +30,8 @@ namespace SuperLogin.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            IsPlayServicesAvailable();
+            CreateNotificationChannel();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -33,5 +40,49 @@ namespace SuperLogin.Droid
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode)) { }
+                else
+                {
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID,
+                                                  "FCM Notifications",
+                                                  NotificationImportance.Default)
+            {
+
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+
+        }
+
     }
+
+
 }
---
+++ SuperLogin.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Util;
+using Firebase.Messaging;
+
+namespace SuperLogin.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+        private readonly string NOTIFICATION_CHANEL_ID = "com.firebase.teste.super_login";
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+
+            if (!message.Data.GetEnumerator().MoveNext())
+            {
+                SendNotification(message.Data);
+            }
+        }
+
+        private void SendNotification(IDictionary<string, string> data)
+        {
+            string title;
+            string body;
+
+            data.TryGetValue("title", out title);
+            data.TryGetValue("body", out body);
+
+
+            SendNotification(title: title, body: body);
+        }
+
+        private void SendNotification(string title, string body)
+        {
+            NotificationManager notificationManager = (NotificationManager)GetSystemService(Context.NotificationService);
+
+            if(Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANEL_ID, "Notification chanel",
+                    Android.App.NotificationImportance.Default);
+
+                notificationChannel.Description = "Super Login Channel";
+                notificationChannel.EnableLights(true);
+                notificationChannel.LightColor = Color.Blue;
+                notificationChannel.SetVibrationPattern(new long[] { 0, 1000, 500, 1000 });
+
+
+                notificationManager.CreateNotificationChannel(notificationChannel);
+
+
+            }
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANEL_ID);
+
+            notificationBuilder.SetAutoCancel(true)
+            .SetDefaults(-1)
+            .SetSmallIcon(Resource.Drawable.abc_ic_star_black_16dp)
+            .SetWhen(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
+            .SetContentTitle(title)
+            .SetContentText(body);
+
+
+            notificationManager.Notify(new Random().Next(), notificationBuilder.Build()); 
+
+        }
+    }
+}
